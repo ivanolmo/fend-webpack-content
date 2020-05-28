ee7074aae0377341367a620d68dedc133c57f63a
Ivan Olmo <ivan.olmo@gmail.com>
2020-05-27 21:29:12 -0500

    Correct issues with eslint and add separate prod/dev config files

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
