20c685c4aeade11aa987f2aac984c2f1722cf3ce
Ivan Olmo <ivan.olmo@gmail.com>
2020-05-28 21:37:24 -0500

    Add BundleAnalyzerPlugin to project

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,5 +22,12 @@ module.exports = {
       template: './src/client/views/index.html',
       filename: './index.html',
     }),
+      new BundleAnalyzerPlugin()
+    // new CleanWebpackPlugin({
+    //   dry: true,
+    //   verbose: true,
+    //   cleanStaleWebpackAssets: true,
+    //   protectWebpackAssets: true
+    // }),
   ],
 };
